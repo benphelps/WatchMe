801619451dc99306ef526d8077123bb619fc41f8
Backburn <jankster@gmail.com>
2014-07-20 14:31:09 -0700

    Follow web standards, always logo links to root.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    if user_signed_in?
-      redirect_to streams_path
-    end
+    --if user_signed_in?
+    --  redirect_to streams_path
+    --end
   end
 
 end
