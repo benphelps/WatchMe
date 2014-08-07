f35d84479e067a52e35b98bc8b499a418db100ab
Benjamin Phelps <bphelpsen@gmail.com>
2014-08-07 08:36:43 -0500

    deploy restart ?

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,14 +5,11 @@ set :scm, :git
 set :repo_url, 'git@gitbin.com:phelps/watchme-io.git'
 
 namespace :deploy do
-
   desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 2 do
       execute :touch, release_path.join("tmp/restart.txt")
     end
   end
-
-  after :finishing, "deploy:cleanup"
-
+  after :finishing, "deploy:restart"
 end
