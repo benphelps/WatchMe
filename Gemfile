145ec7d5e0fb7675b98eb0af9614fc46c030d48e
Benjamin Phelps <bphelpsen@gmail.com>
2014-08-07 02:34:48 -0500

    postgres

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
-
+gem 'pg'
 gem 'mustache-js-rails'
 gem 'gistyle'
 gem 'gon'
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,3 @@ set :deploy_user, 'deploy'
 
 set :scm, :git
 set :repo_url, 'git@gitbin.com:phelps/watchme-io.git'
-
-set :linked_dirs, %w{bin log tmp vendor/bundle public/system certs}
-
-namespace :deploy do
-  task :restart, roles: :app, except: { no_release: true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
