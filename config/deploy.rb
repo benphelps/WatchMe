f210a651b27729d6685c38d5994b1ed95acb0b6f
Benjamin Phelps <bphelpsen@gmail.com>
2014-08-08 04:45:56 -0500

    increase log level

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,6 @@ set :deploy_user, 'deploy'
 set :scm, :git
 set :repo_url, 'git@gitbin.com:phelps/watchme-io.git'
 
-set :log_level, :info
-
 set :linked_dirs, %w{bin log tmp vendor/bundle public/system public/uploads}
 
 namespace :deploy do
