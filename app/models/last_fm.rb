25cc31b0254ee07f288aae4fd01b37336b803c00
Benjamin Phelps <bphelpsen@gmail.com>
2014-08-10 16:48:44 -0500

    add musicbrainz IDs to the LastFm responses

--- app/models/last_fm.rb
+++ app/models/last_fm.rb
@@ -2,13 +2,18 @@ class LastFm < ActiveRecord::Base
   
   def self.nowplaying(user)
     recent = recent(user)
-    if recent['recenttracks']['track'][0]
+    if recent['recenttracks'] && recent['recenttracks']['track'] && recent['recenttracks']['track'][0]
       return {
         playing: ( recent['recenttracks']['track'][0]['@attr'] && recent['recenttracks']['track'][0]['@attr']['nowplaying'] ? true : false ),
         name: recent['recenttracks']['track'][0]['name'],
         artist: recent['recenttracks']['track'][0]['artist']['#text'],
         album: recent['recenttracks']['track'][0]['album']['#text'],
         image: recent['recenttracks']['track'][0]['image'].last['#text'],
+        mbid: {
+          artist: recent['recenttracks']['track'][0]['artist']['mbid'],
+          album: recent['recenttracks']['track'][0]['album']['mbid'],
+          track: recent['recenttracks']['track'][0]['mbid']
+        },
         amazon: amazon(recent['recenttracks']['track'][0]['name'], recent['recenttracks']['track'][0]['artist']['#text']),
         spotify: spotify(recent['recenttracks']['track'][0]['name'], recent['recenttracks']['track'][0]['artist']['#text'],recent['recenttracks']['track'][0]['album']['#text'])
       }
@@ -18,7 +23,6 @@ class LastFm < ActiveRecord::Base
       }
     end
   end
-  
   def self.recent(user)
     Rails.cache.fetch([user, 'lastfm'], :expires_in => 5.seconds) do
       json = HTTParty.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=2&user=#{user}&api_key=b002d72c627d2b046f7326a31c069f7d&format=json")
@@ -26,12 +30,13 @@ class LastFm < ActiveRecord::Base
     end
   end
   
+  # Spotify
+  
   def self.spotify(title, artist, album)
     Rails.cache.fetch([title, artist, album, 'spotify'], :expires_in => 1.day) do
       query_spotify(title, artist, album)
     end
   end
-  
   def self.query_spotify(title, artist, album)
     json = HTTParty.get("http://api.spotify.com/v1/search?q=track:#{URI::encode(title)}%20artist:#{URI::encode(artist)}%20album:#{URI::encode(album)}&type=track")
     if json.parsed_response['tracks']['total'] > 0
@@ -41,12 +46,13 @@ class LastFm < ActiveRecord::Base
     end
   end
   
+  # Amazon
+  
   def self.amazon(title, artist)
     Rails.cache.fetch([title, artist, 'amazon'], :expires_in => 1.day) do
       query_amazon(title, artist)
     end
   end
-  
   def self.query_amazon(title, artist)
     req = Vacuum.new
     req.configure(
