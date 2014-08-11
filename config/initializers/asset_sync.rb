113b98940825b1ab47112aa50dc5929aa7d10252
Benjamin Phelps <bphelpsen@gmail.com>
2014-08-11 13:00:03 -0500

    some more asset settings

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,5 +4,6 @@ AssetSync.configure do |config|
   config.aws_secret_access_key = Settings.aws.secret
   config.fog_directory = Settings.aws.bucket
   config.manifest = true
+  config.existing_remote_files = 'delete'
   #config.gzip_compression = true
 end
