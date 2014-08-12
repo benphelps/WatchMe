3dc67a7e89d61193927b29eee3d2aa13baa65c16
Benjamin Phelps <bphelpsen@gmail.com>
2014-08-12 16:09:46 -0500

    Lets not use S3 here.

--- app/uploaders/placeholder_uploader.rb
+++ app/uploaders/placeholder_uploader.rb
@@ -4,7 +4,7 @@ class PlaceholderUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
-  storage :fog
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
